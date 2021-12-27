53dd1d10c27f42d331efb114fda34183e7b7a24f
rriiaad <85185705+rriiaad@users.noreply.github.com>
2021-12-27 13:09:54 +0000

    Add files via upload

--- challange 2/loged.php
+++ challange 2/loged.php
@@ -1,3 +1,34 @@
     <?php
-      echo $_SESSION["user"];
+      session_start();
     ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $_SESSION["user"]; ?></title>
+    </head>
+
+    <body>
+
+        <?php
+      if(strcasecmp($_SESSION["user"],"user")==1){
+        echo "<button>Create an article</button>";
+
+      }else if(strcasecmp($_SESSION["user"],"admin")==1){
+        echo "<button>Create an article</button>
+              <button>Modify an article</button>";
+        
+
+      }else if(strcasecmp($_SESSION["user"],"superAdmine")==1) {
+        echo "<button>Create an article</button>
+              <button>Modify an article</button>
+              <button>Delete an article</button>";
+      }
+        ?>
+    </body>
+
+    </html>
